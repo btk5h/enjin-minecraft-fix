39d9491f41b4b484393fb8c336a606134d0538c1
Bryan Terce <bryan.terce@gmail.com>
2016-03-24 22:07:25 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# enjin-minecraft-fix
+Enjin support for Minecraft 1.8 sucks. This Chrome extension fixes that by displaying user skins with 1.8 layers.
+
+## How does it work?
+Enjin convenient marks its avatars with the `minecraft-model` class and uses the `data-name` attribute to store the username. This extension replaces those avatars using [Crafatar](https://crafatar.com/)'s awesome skin rendering service.
---
+++ src/patch.js
@@ -0,0 +1,33 @@
+'use strict'
+
+for (let canvas of document.getElementsByClassName('minecraft-model')) {
+  getUUID(canvas.getAttribute('data-name'), uuid => {
+    let image = new Image()
+    image.onload = () => {
+      canvas.parentNode.replaceChild(image, canvas)
+    }
+    image.src = `https://crafatar.com/renders/body/${uuid}?overlay&scale=3`
+  })
+}
+
+function getUUID (name, callback) {
+  chrome.storage.local.get(name, result => {
+    let uuid = result[name]
+    if (uuid == null) {
+      let request = new XMLHttpRequest()
+      request.open('GET', `https://api.mojang.com/users/profiles/minecraft/${name}`, true)
+      request.onreadystatechange = () => {
+        if (request.readyState == 4) {
+          let id = JSON.parse(request.responseText).id
+          let map = {}
+          map[name] = id
+          chrome.storage.local.set(map)
+          callback(id)
+        }
+      }
+      request.send()
+    } else {
+      callback(uuid)
+    }
+  })
+}
